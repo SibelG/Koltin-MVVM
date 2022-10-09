ca2984294396b3e34394f31612950fd6d5f71367
Sibel GENÇTÜRK <46397935+SibelG@users.noreply.github.com>
2022-10-09 16:32:45 +0300

    Initial commit

---
+++ app/src/main/java/com/app/koltinpoc/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.app.koltinpoc.utils
+
+object Constants {
+
+    val API_KEY = "6953a7421bb749c589e687746c85eee8" //GET YOUR API KEY BY SIGNING to https://newsapi.org
+    val BASE_URL = "https://newsapi.org/"
+    val COUNTRY_CODE = "in"
+
+}
---
+++ app/src/main/java/com/app/koltinpoc/view/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.app.koltinpoc.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.app.koltinpoc.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.concurrent.fixedRateTimer
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navigationHost: NavHostFragment
+    lateinit var bottomNav : BottomNavigationView
+    lateinit var bottomNavView : NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigationHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        bottomNavView = navigationHost.navController
+        bottomNav=findViewById(R.id.bottomNavigationView)
+        //bottomNavView=findNavController(R.id.nav_host_fragment_container)
+
+        bottomNav.setupWithNavController(bottomNavView)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.breakingNewsFragment,
+                R.id.savedNewsFragment
+
+            )
+        )
+        setupActionBarWithNavController(bottomNavView, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return bottomNavView.navigateUp(appBarConfiguration)
+    }
+}
