65216d153cf251fdf94981f4c88fc93847038b03
Sibel GENÇTÜRK <46397935+SibelG@users.noreply.github.com>
2023-01-01 03:36:45 +0300

    api replaced

--- app/src/main/java/com/app/koltinpoc/utils/Constants.kt
+++ app/src/main/java/com/app/koltinpoc/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.app.koltinpoc.utils
 
 object Constants {
 
-    val API_KEY = "6953a7421bb749c589e687746c85eee8" //GET YOUR API KEY BY SIGNING to https://newsapi.org
+    val API_KEY = "34a157192c7b4867ae7690ab97438ca8" //GET YOUR API KEY BY SIGNING to https://newsapi.org
     val BASE_URL = "https://newsapi.org/"
     val COUNTRY_CODE = "tr"
     const val SEARCH_NEWS_TIME_DELAY = 500L
